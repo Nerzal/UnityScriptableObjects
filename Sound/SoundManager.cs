26bd886c751bb1c228751073c3e61c72053c7192
Nerzal <info@noobygames.de>
2020-04-05 20:49:05 +0200

    Added soundmanager, that works as receiver for playsound events

---
+++ Sound/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿
+using Events;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundManager : MonoBehaviour {
+        [SerializeField] private AudioSource _audioSource;
+
+        public void OnSound(PlaySoundEventArgs args) {
+            if (this.transform.GetInstanceID() != args.ReceiverInstanceID) {
+                return;
+            }
+
+            this._audioSource.PlayOneShot(args.Sound);
+        }
+    }
+}
